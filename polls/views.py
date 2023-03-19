e9a746565d15c0d500239045ad7b5e0c3173a911
김민석 <alstjr6823@naver.com>
2023-03-19 20:51:44 +0900

    뷰추가가

--- polls/views.py
+++ polls/views.py
@@ -5,11 +5,23 @@ from django.http import HttpResponse
 from .models import Question #models.py에Question 함수 있음
 # Create your views here.
 def index(request):
-    questions =Question.objects.all()
-    #str=''
-    #for question in questions:
-    #   str += "{} 날짜 : {} <br/>".format(question.question_txt,question.pub_date)
-    #   str += "--------------<br/>"
-    #return HttpResponse(str)
-    context = {'questions': questions}
-    return render(request, 'temp_test/index.html', context)
+    #questions =Question.objects.all()
+        #str=''
+        #for question in questions:
+        #   str += "{} 날짜 : {} <br/>".format(question.question_txt,question.pub_date)
+        #   str += "--------------<br/>"
+        #return HttpResponse(str)
+    #context = {'questions': questions}
+    #return render(request, 'temp_test/index.html', context)
+    latest_question_list =Question.objects.order_by('-pub_date')
+    output = ', '.join([q.question_txt for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request,question_id):
+    return HttpResponse("당신은 %s번 질문을 보고 있습니다." %question_id)
+
+def result(request, question_id):
+    return HttpResponse("당신은 %s번 질문의 결과를 보고 있습니다." %question_id)
+
+def vote(request, question_id):
+    return HttpResponse("당신은 %s번 질문에 투표를 합니다." %question_id)
